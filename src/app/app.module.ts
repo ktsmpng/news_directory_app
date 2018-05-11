f5d8a2cdaefd6a62d72c0c3a0371c0bcd179b6b1
Kitty <kitsumpang@gmail.com>
2018-05-11 19:14:05 +0100

    binded pages to buttons in home.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { NewsPage} from '../pages/news/news';
+import { SettingsPage} from '../pages/settings/settings';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    NewsPage,
+    SettingsPage,
   ],
   imports: [
     BrowserModule,
@@ -19,7 +23,9 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    NewsPage,
+    SettingsPage,
   ],
   providers: [
     StatusBar,
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { NewsPage} from '../news/news';
+import { SettingsPage} from '../settings/settings';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
+  newsPage = NewsPage;
+  settingsPage = SettingsPage;
   constructor(public navCtrl: NavController) {
 
   }
